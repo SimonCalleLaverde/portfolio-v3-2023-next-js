95331a0ed71bd93cf4163905ae5dce85afd1acdf
Simón Calle Laverde <kyverde88@gmail.com>
2023-02-16 23:30:20 -0500

    Installing "Bootstrap 5" Using CDN (For Now).
    -- For now "Using CDN links".
    -- Will "Install using NPM" soon.
    º By "npm i bootstrap".
    º Bringing CSS & using "useEffect" to bring JS.
    
    - As per:
    º "https://dev.to/anuraggharat/adding-bootstrap-to-nextjs-39b2".
    
    - CDN Links (Bootstrap Official):
    º "https://getbootstrap.com/docs/5.0/getting-started/introduction/".

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 // Imports
+import Head from "next/head"
+import Script from "next/script"
 // Adding "main.scss" (Global Or General) CSS (Can Only Within This "_app.js")
 import "../styles/main.scss";//import "../styles/globals.scss";
 
 // Main Entry Point Into The App (Every Individual Page Will Start From This Template)
 export default function PortfolioV32023NextJS({ Component, pageProps }) {
-  return <Component {...pageProps}/>
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      </Head>
+
+      {/* BOOTSTRAP 5 (JS & POPPER) */}
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"/>
+
+      <Component {...pageProps}/>
+    </>
+  )
 };
--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,22 @@
-// Overriding The Default "Document"
+// OVERRIDING THE DEFAULT "DOCUMENT"
 
 // Imports
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      {/*The <Head/> component used here should only be
-      used for any <head> code that is common for all pages.
-      For all other cases, such as <title> tags, we recommend
-      using next/head in your pages or components.*/}
+      {/*The <Head/> component used here should only be used for any <head> code that is common for all pages.
+      For all other cases, such as <title> tags, we recommend using next/head in your pages or components.*/}
 
       {/*<Head/>*/}
       <Head>
+        {/* BOOTSTRAP 5 (CSS) */}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"/>
+
         {/* TERMINAL's WARNING/STEPS-FOLLOWED */}
         {/* Do not add stylesheets using next/head (see <link rel="stylesheet"> tag with href="https://db.onlinewebfonts.com/c/4c4c08af466e9ad071b6d69cf44093df?family=Saol+Display+Regular"). Use Document instead. 
         See more info here: https://nextjs.org/docs/messages/no-stylesheets-in-head-component */}
-
         <link href="https://db.onlinewebfonts.com/c/4c4c08af466e9ad071b6d69cf44093df?family=Saol+Display+Regular" rel="stylesheet" type="text/css"/>
       </Head>
 
