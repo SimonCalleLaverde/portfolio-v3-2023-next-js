5e6c3f9aac7ba1cbe001c15b8e315204adaca626
Simón Calle Laverde <kyverde88@gmail.com>
2023-05-09 00:35:25 -0500

    Production Changes.
    - Changing from "ñeña" to "nena" in image filenames and image src for the "Cube 3D", for 'production' as image disappeared from live site.
    -- Finally live/deployed in "simonuxui.vercel.app" again!

--- components/Cube3D.js
+++ components/Cube3D.js
@@ -12,13 +12,13 @@ export default function Cube3D() {
         <div className="face top background-image-default" type="image" style={{ backgroundImage:"url('/profile-images/home-header-picture-ii-(cropped).jpg')" }} title="Simón Calle Laverde and his cat Ñeña in a pure HTML/CSS 3D rotating cube" aria-hidden="true">
         </div>
 
-        <div className="face right background-image-default" type="image" style={{ backgroundImage:"url('/profile-images/ñeña-facebook-video-screenshot-(cropped).png')" }} title="Simón Calle Laverde and his cat Ñeña in a pure HTML/CSS 3D rotating cube" aria-hidden="true">
+        <div className="face right background-image-default" type="image" style={{ backgroundImage:"url('/profile-images/nena-facebook-video-screenshot-(cropped).png')" }} title="Simón Calle Laverde and his cat Ñeña in a pure HTML/CSS 3D rotating cube" aria-hidden="true">
         </div>
 
         <div className="face bottom background-image-default" type="image" style={{ backgroundImage:"url('/profile-images/home-header-picture-ii-(cropped).jpg')" }} title="Simón Calle Laverde and his cat Ñeña in a pure HTML/CSS 3D rotating cube" aria-hidden="true">
         </div>
 
-        <div className="face left background-image-default" type="image" style={{ backgroundImage:"url('/profile-images/ñeña-facebook-video-screenshot-(cropped).png')" }} title="Simón Calle Laverde and his cat Ñeña in a pure HTML/CSS 3D rotating cube" aria-hidden="true">
+        <div className="face left background-image-default" type="image" style={{ backgroundImage:"url('/profile-images/nena-facebook-video-screenshot-(cropped).png')" }} title="Simón Calle Laverde and his cat Ñeña in a pure HTML/CSS 3D rotating cube" aria-hidden="true">
         </div>
 
         <div className="face back background-image-default" type="image" style={{ backgroundImage:"url('/profile-images/home-header-picture-i-(cropped).jpg')" }} title="Simón Calle Laverde and his cat Ñeña in a pure HTML/CSS 3D rotating cube" aria-hidden="true">
