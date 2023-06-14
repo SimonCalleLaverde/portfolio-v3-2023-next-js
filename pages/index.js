6a25a5e48027a2357ea492652f6fd7a0f8e20b68
Simón Calle Laverde <kyverde88@gmail.com>
2023-06-14 18:25:47 -0500

    Uploading More "Case Studies".
    - Uploaded/Created "Calle Impresores" project (finally, OMG, how long).
    - And "targeting" my ¿main fear I guess? (OMG again, the reason why I (lazily) let just "1" project in the Home Page at the moment last time. My main project wasn't in order as first, and was lazy yet to see how to order them. So I had just hidden the other for now).
    º Finally! It was pretty easy as per "https://hygraph.com/docs/api-reference/content-api/ordering" and improvising a bit.

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,16 @@ const accessEndpoint = "https://api-us-east-1.hygraph.com/v2/cl5ketcvx2wnm01ta90
 const graphCMSRequestAPI = new GraphQLClient(accessEndpoint);
 
 // Querying (Projects) With GraphQL //projects(where: { title_contains: "..." }) { //projects(where: { slug_contains: "..." }) { //projects(where: { slug: "jomaira-imagen" }) { //projects(where: { category_contains: "Recent Independent Project" }) { //projects(where: { roles_contains_all: "UX Research" }) { //projects(where: { type_contains: "Coaching Website Re-Design" }) { //projects(where: { tags_contains_all: "Home" }) {
+// {
+//   posts(orderBy: createdAt_ASC) {
+//     id
+//     createdAt
+//   }
+// }
+
 const graphCMSQueryProjects = gql`
   {
-    projects(where: { tags_contains_all: "Home" }) {
+    projects(where: { tags_contains_all: "Home" }, orderBy: order_ASC) {
 
       id
       slug
