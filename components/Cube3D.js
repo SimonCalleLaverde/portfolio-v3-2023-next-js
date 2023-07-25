20e829d1e99f7f16c08b90bb37bf0ca21bfb4155
Simón Calle Laverde <kyverde88@gmail.com>
2023-07-25 17:56:22 -0500

    Adding New Pic To "Cube3D".

--- components/Cube3D.js
+++ components/Cube3D.js
@@ -9,7 +9,7 @@ export default function Cube3D() {
         <div className="face front background-image-default" type="image" style={{ backgroundImage:"url('/home-page-images/home-header-picture-i-(cropped)-(tinypng).jpg')" }} title="Simón Calle Laverde and his cat Ñeña in a pure HTML & CSS 3D rotating cube">
         </div>
 
-        <div className="face top background-image-default" type="image" style={{ backgroundImage:"url('/home-page-images/home-header-picture-ii-(cropped)-(tinypng).jpg')" }} title="Simón Calle Laverde and his cat Ñeña in a pure HTML & CSS 3D rotating cube" aria-hidden="true">
+        <div className="face top background-image-default" type="image" style={{ backgroundImage:"url('/home-page-images/home-header-picture-iii-(portfolio-2021-pic)-(original)-(tinypng).jpg')" }} title="Simón Calle Laverde and his cat Ñeña in a pure HTML & CSS 3D rotating cube" aria-hidden="true">
         </div>
 
         <div className="face right background-image-default" type="image" style={{ backgroundImage:"url('/home-page-images/nena-facebook-video-screenshot-(cropped)-(tinypng).png')" }} title="Simón Calle Laverde and his cat Ñeña in a pure HTML & CSS 3D rotating cube" aria-hidden="true">
